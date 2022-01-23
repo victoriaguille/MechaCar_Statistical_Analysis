2bd59660fe75980d6c915c24dbbab7171ae382d1
victoriaguille <91439219+victoriaguille@users.noreply.github.com>
2022-01-22 21:45:19 -0500

    Update MechaCarChallenge.R
    performed ttests for all lots and individual lots for PSI

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,23 @@ total_summary <- summarize(suscoil, Mean=mean(PSI), Median=median(PSI), Variance
 
 #create a grouped by lot summary dataframe
 lot_summary <- suscoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = "keep")
+
+#perform a t-test for all of psi data
+total_test <- t.test(suscoil$PSI, mu = 1500)
+#print the results
+total_test
+
+#perform a t-test for lot 1 of psi data
+lot1_test <- t.test(subset(suscoil$PSI, suscoil$Manufacturing_Lot == "Lot1"), mu = 1500)
+#print the results
+lot1_test
+
+#perform a t-test for lot 2 of psi data
+lot2_test <- t.test(subset(suscoil$PSI, suscoil$Manufacturing_Lot == "Lot2"), mu = 1500)
+#print the results
+lot2_test
+
+#perform a t-test for lot 3 of psi data
+lot3_test <- t.test(subset(suscoil$PSI, suscoil$Manufacturing_Lot == "Lot3"), mu = 1500)
+#print the results
+lot3_test
